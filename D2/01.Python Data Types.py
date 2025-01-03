70feea18b3c327d879507364380906d0061a7296
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-03 15:53:31 +0500

    Data Types

---
+++ D2/01.Python Data Types.py
@@ -0,0 +1,19 @@
+#Data Types
+#Strings
+print("Hello" [0])   #Concept Known as Subscripting
+print("Hello" [-1])
+print("Hello" [4]) # or 
+
+
+print ("1234"+"5678") #Concatenation and numbers are not treated as integers
+
+#Integers
+print(123+456) 
+print(123_567) #Python ignores the underscore and treats it as in maths 123,567 or 123567 
+
+#Floats
+print(3.14)
+
+#Booleans           
+print(True) 
+print(False)
