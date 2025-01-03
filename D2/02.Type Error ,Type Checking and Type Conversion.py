286ff8844334777ac6075113fa3222c7961ca4e2
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-03 17:34:26 +0500

    Added Type Error ,Type Checking and Type Conversion or casting

---
+++ D2/02.Type Error ,Type Checking and Type Conversion.py
@@ -0,0 +1,15 @@
+num_Char=len(input("Please Enter your name: ")) 
+print("The number of characters in your name is", num_Char)
+print("The Data type of Your name is ",type(num_Char))
+
+#But We Face an Error when we do cancatenation with a string and an integer
+#So we need to convert the integer to a string
+#We can do this by using the str() function
+
+#print("The number of characters in your name is: "+num_Char+ "characters") 
+#This will give an error
+
+num_char=str(num_Char) #convert the integer to a string [Type Conversion or Type Casting]
+print("Your name has " +num_char+  "  characters") #This will work
+
+ 
