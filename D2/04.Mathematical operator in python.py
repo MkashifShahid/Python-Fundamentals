22347f0e0440b882c3e615c00732306d116ec31e
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-03 20:24:01 +0500

    Add Mathematical operator[PEMDAS Rule]

---
+++ D2/04.Mathematical operator in python.py
@@ -0,0 +1,10 @@
+# PEMDAS   Parentheses, Exponents, Multiplication and Division (from left to right), Addition and Subtraction (from left to right)
+# 1. Parentheses
+# 2. Exponents
+# 3. Multiplication or Division (from left to right)
+# 4. Addition or Subtraction (from left to right)
+
+print(3 * 3 + 3/3 - 3)
+
+print(3 * (3 + 3)/3 - 3) 
+
