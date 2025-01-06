e8f4a998a15ce6f08079d9cc9771e7faba6e0e24
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-06 17:00:16 +0500

    Tim Urban's article, Your Life in Weeks (https://waitbutwhy.com/2014/05/life-weeks.html), highlights how little time we truly have. Inspired by this, the task is to create a program that calculates and displays the time left if we live up to 90 years. The program should use math and Python f-strings to determine the remaining days, weeks, and months based on the user's current age, and output a message in this format: You have x days, y weeks, and z months left. Here, x, y, and z are replaced with the calculated numbers. The output format must strictly match the example provided, including commas and full stops.

--- D2/07.Exercise[Life in weeks].py
+++ D2/07.Exercise[Life in weeks].py
@@ -3,4 +3,4 @@ days=int(years)*365
 month=int(years)*12
 weeks=int(years)*52
 years=90-int(years)
-print(f"You have {days} days, {weeks} weeks, and {month} months left")# You life in weeks
+print(f"You have {days} days, {weeks} weeks, and {month} months left")
