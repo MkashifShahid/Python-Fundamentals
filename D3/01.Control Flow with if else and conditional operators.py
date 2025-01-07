2863c6b241bbafb125858658b12f3952dd6751cf
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-07 16:54:00 +0500

    DAY 02 COMPLETED WITH SMALL PROJECT

--- D3/01.Control Flow with if else and conditional operators.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the rollercoaster!")
-
-hight=int(input("please Enter your Hight in cm:"))
-if hight>120:
-    print("You can ride the rollercoaster!")  
-    
-else:
-    print("Sorry, you have to grow taller before you can ride.")
-
