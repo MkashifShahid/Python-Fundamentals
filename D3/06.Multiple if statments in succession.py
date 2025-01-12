173f4f1be8448b4fd3965cb281f584b669966b65
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-12 12:27:58 +0500

    Rollercoaster 2.0 using Multiple if statments in succession

---
+++ D3/06.Multiple if statments in succession.py
@@ -0,0 +1,25 @@
+print("Welcome to The Roller Coaster!")
+height=float(input("Please Enter Your Height in cm:"))
+bill=0
+if height >= 120:
+    print("You can ride the Roller coaster!")
+    age=float(input("Please Enter Age :"))
+    if age <12:
+        bill=5
+        print(f"Child Ticket are {bill}$.")
+    elif age <=18:
+        bill=7
+        print(f"Youth tickets are {bill}$. ")
+    else:
+        bill=12
+        print(f"Adult tickets are {12}$.")
+    want_photo=input("Do you want a photo Taken? Y or N :")
+    if want_photo=="Y" or want_photo=="y":
+        
+        # bill=bill+3
+        bill+=3   
+        print(f"Your final Bill is {bill}$ ")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+    
