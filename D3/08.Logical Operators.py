11ab3b0577c0467b41b778e51535bce8da7f4faa
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-13 06:54:25 +0500

    Added RollerCoaster 3.0 using Logical Operators

--- D3/08.Logical Operators.py
+++ D3/08.Logical Operators.py
@@ -1,4 +1,4 @@
-print("Welcome to The Roller Coaster 2.0!")
+print("Welcome to The Roller Coaster 3.0!")
 height=float(input("Please Enter Your Height in cm:"))
 bill=0
 if height >= 120:
@@ -10,17 +10,20 @@ if height >= 120:
     elif age <=18:
         bill=7
         print(f"Youth tickets are {bill}$. ")
-    else:
+    elif age >18 and age <45:
         bill=12
         print(f"Adult tickets are {12}$.")
+    elif age >=45 and age <=50:
+        bill=0
+        print("Congratulations! You are eligible for a free ride.")
+    else:
+     print("Invalid option. Please Try again!.")
+
     want_photo=input("Do you want a photo Taken? Y or N :")
-    if want_photo=="Y" or want_photo=="y":
-        
+    if age<51 and want_photo=="Y" or want_photo=="y":
         # bill=bill+3
         bill+=3   
         print(f"Your final Bill is {bill}$ ")
-    else:
-        print(f"Your Final Bill {bill}$")
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
