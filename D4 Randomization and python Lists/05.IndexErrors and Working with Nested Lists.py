8fbffb7860ad45b34293b5bc11db8db428d7fa0b
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-15 10:28:12 +0500

    Add nested list for produce

---
+++ D4 Randomization and python Lists/05.IndexErrors and Working with Nested Lists.py
@@ -0,0 +1,12 @@
+
+#01.Index Error
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+print(dirty_dozen)
+print(len(dirty_dozen))
+# print(dirty_dozen[12]) #IndexError: list index out of range
+
+#02.Nested Lists
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen1 = [fruits, vegetables]
+print(dirty_dozen1)
