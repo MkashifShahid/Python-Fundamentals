49df2042d0250ad0b2913b3565bd60ce3f4faa41
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-15 09:36:39 +0500

    Banker Roulette - Who will pay the bill

--- D4 Randomization and python Lists/04.Banker Roulette - Who will pay the bill.py
+++ D4 Randomization and python Lists/04.Banker Roulette - Who will pay the bill.py
@@ -1,19 +1,13 @@
 import random
 
-# Prompt the user to input names separated by commas
-name_string = input("Give me everybody's names, separated by a comma: ")
+# Get names from the user
+name_string = input("Enter names separated by commas: ")
 
-# Split the input string into a list of names using ',' as the delimiter
+# Split names into a list
 names = name_string.split(",")
 
-# Get the length of the list of names
-length_of_names = len(names)
+# Pick a random name
+bill_payer = random.choice(names)
 
-# Randomly select an index from 0 to length_of_names - 1
-random_choice = random.randint(0, length_of_names - 1)
-
-# Get the name at the randomly selected index
-bill_payer = names[random_choice].strip()
-
-# Print the name of the person who will pay the bill
+# Print the result
 print(f"{bill_payer} will pay the bill today!")
