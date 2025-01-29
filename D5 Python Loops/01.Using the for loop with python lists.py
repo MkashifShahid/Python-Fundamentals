c23e2fb8c01d7309de7f095347fc6cb5bc0c4b6e
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-29 14:16:54 +0500

    Using for loop with python lists

---
+++ D5 Python Loops/01.Using the for loop with python lists.py
@@ -0,0 +1,5 @@
+fruits= ["Apple","Mango", "Banana"]
+for kashif in fruits:  # Here kashif work as a variable. In loop first time kashif= apple and so on.. 
+    print(kashif)      
+    # print(kashif+"Pie")
+    # print(fruits)
