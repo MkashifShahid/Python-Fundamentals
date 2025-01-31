e208f361c11108d74fef2b41ebf3a37157d9d461
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-31 09:37:02 +0500

    Program that calculates the sum of all the even numbers from 1 to 100, including 1 and 100.

--- D5 Python Loops/06.Adding Even NumbersExercise.py
+++ D5 Python Loops/06.Adding Even NumbersExercise.py
@@ -1,4 +1,12 @@
+#sol :01
 total=0
 for Number in range (2,101,2):
     total+=Number
 print(f"The Sum of Even Numbers is {total}")
+
+#sol :02
+total1=0
+for number in range (1,101):
+    if number%2==0:
+        total1+=number
+print(f"The sum of Even Numbers is {total1}")
