fad79ef382e7b61b46f9ee71b3564470539ad0cd
MkashifShahid <bsf2101016@ue.edu.pk>
2025-01-31 09:16:46 +0500

    For loops and Range function with The code that  will calculate the sum of numbers from 1 to 100 and print the final sum (which is 5050) only once.

---
+++ D5 Python Loops/05.For loops and Range() function.py
@@ -0,0 +1,16 @@
+# [.....................For loop With Range............]
+#01 
+# for number in range(1,11):
+#     print(number)
+    
+# #02
+# for number in range(1,11,5):
+#     print(number)
+
+#3 Sum of 1 to 100 
+total = 0
+for sum in range(1, 101):
+    total += sum
+
+print(f"The total Sum is {total}")
+    
