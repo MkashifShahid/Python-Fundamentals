977bdc2d6aba059a15cf6ef9a40b60460d1e03b5
MkashifShahid <bsf2101016@ue.edu.pk>
2025-02-01 11:57:20 +0500

    indentation

---
+++ D6 Python Functions & Karel/02.indentation.py
@@ -0,0 +1,11 @@
+sky = "clear"  # Define the variable 'sky'
+
+def my_function():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("Hello")
+
+print("World")
+my_function()  # Call the function with parentheses
