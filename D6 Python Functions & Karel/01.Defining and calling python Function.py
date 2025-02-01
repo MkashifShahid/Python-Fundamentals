67421ec6325577a32d79c7a345896b9566c9e58b
MkashifShahid <bsf2101016@ue.edu.pk>
2025-02-01 10:52:21 +0500

    Defining and calling python Function

---
+++ D6 Python Functions & Karel/01.Defining and calling python Function.py
@@ -0,0 +1,19 @@
+# Built-in function example
+# This line prints the string "Hello" to the console
+print("Hello")
+
+# This line calculates the length of the string "Hello" using the built-in len() function and stores it in the variable 'length'
+length = len("Hello")
+
+# This line prints the value of the variable 'length' to the console
+print(length)
+
+# Defining a custom function named 'my_function'
+def my_function():
+    # This line prints the string "Hello" to the console
+    print("Hello")
+    # This line prints the string "bye" to the console
+    print("bye")
+
+# Calling the custom function 'my_function'
+my_function()
