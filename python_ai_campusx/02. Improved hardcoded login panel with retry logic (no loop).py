b161dcb5bf24e39c48fa609e9f91c9c8e9d78ef8
MkashifShahid <bsf2101016@ue.edu.pk>
2025-11-01 01:07:48 +0500

    Improved hardcoded login panel with retry logic (no loop)

---
+++ python_ai_campusx/02. Improved hardcoded login panel with retry logic (no loop).py
@@ -0,0 +1,16 @@
+email = input("Please Enter your Email: ")
+passwd = input("Please Enter Passwd: ")
+if"@" in email:
+    if email == "admin@gmail.com" and passwd == "1234":
+     print("Welcome User")
+    elif email == "admin@gmail.com" and passwd != "1234":
+        print("Wrong password.Try Again!")
+        newpass=input("Please enter your password again!")
+        if newpass=="1234":
+        print("Finally Welcome User!")
+    else:
+        print("429:Too many Requests!") #Aim :without Loop 
+
+else:
+    print("Invalid email or password. Please try again.")
+
