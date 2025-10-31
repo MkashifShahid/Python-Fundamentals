578906e852cc162e2321dc2b6d65d81def351eb5
MkashifShahid <bsf2101016@ue.edu.pk>
2025-10-31 21:42:42 +0500

    Added simple hardcoded login panel script

---
+++ python_ai_campusx/simple hardcoded login panel.py
@@ -0,0 +1,8 @@
+email=input("Please Enter your Email:")
+passwd=input("Please Enter Passwd:")
+if email=="admin@gmail.com" and passwd=="1234":
+    print("welcome User")
+else:
+    print("Invalid email or password. Please try again.")
+
+    
