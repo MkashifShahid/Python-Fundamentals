607f93a8426e1165ae697ef33d26dd6590b8b41e
MkashifShahid <bsf2101016@ue.edu.pk>
2025-11-01 13:54:04 +0500

    Loops(While & for)

---
+++ python_ai_campusx/02. Loops.py
@@ -0,0 +1,16 @@
+print("___ 'WHILE LOOP'_____")
+number=int(input("Please Enter Your Number:"))
+i=1
+while i!=11:
+    print(number*i)
+    i=i+1
+
+print("___ 'FOR LOOP'_____")
+for i in range(15,25,2):
+    print(i)
+
+for i in range(10,0,-1):
+    print(i)
+
+for i in range(0,6,2):
+    print("karachi")
