a5afe81d3bde3a2bc623e8a6d8d75bf093a4a697
MkashifShahid <bsf2101016@ue.edu.pk>
2025-11-03 08:36:25 +0500

    List Operations

---
+++ python_ai_campusx/07. List Operations.py
@@ -0,0 +1,19 @@
+L1=[1,2,3,4,5]
+L2=L1*2
+print(L2) 
+L3=[6,"Kashif","Ali"]
+l=L1+L3
+print(l)
+
+x="la" in "lahore"   #Membership Operator
+print(x)
+x2= 1 in L1
+print(x2)
+
+L5=[[2,3],[5,9],[6,0]]
+l5=[2,3] in L5
+print(l5)
+
+List7=["Lahore",1,2,3]
+for char in List7:
+    print(char)
