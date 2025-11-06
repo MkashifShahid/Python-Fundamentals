9d617a231dc1a2f8c48269c1854a0cbee1bc73fa
MkashifShahid <bsf2101016@ue.edu.pk>
2025-11-07 00:08:34 +0500

    Simple Dictionary and operations

--- python_ai_campusx/16. Dictionary.py
+++ python_ai_campusx/16. Dictionary.py
@@ -2,3 +2,16 @@ dic={"Name":"kashif","age":21,"city":"Lahore"}
 print(dic)
 type=type(dic)
 print(type)
+print(dic["Name"])
+print(dic["age"])
+dic["Name"]="Muhammad kashif shahid"
+print("Updated Dic is=",dic)
+dic["Nationality"]="Pakistan"
+print(dic)
+del dic["age"]
+print(dic)
+B="city" in dic  #Membership Operator
+print(B)
+
+for i in dic:
+    print(i,"=",dic[i])
