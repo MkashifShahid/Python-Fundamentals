989c72ccc90667d06225fbe2dad9ca87fac6efe8
muhammadkashifsecurityengineer <bsf2101016@ue.edu.pk>
2025-11-11 22:47:53 +0500

    Verification

--- python_ai_campusx/simple hardcoded login panel.py
+++ python_ai_campusx/simple hardcoded login panel.py
@@ -2,7 +2,7 @@ email=input("Please Enter your Email:")
 passwd=input("Please Enter Passwd:")
 if email=="admin@gmail.com" and passwd=="1234":
     print("welcome User")
-else:
-    print("Invalid email or password. Please try again.")
+else:  
+    print("Invalid email or password. Please try again.") #hi
 
     
